002cbd8770f034ac09e50f156d68fba807728e18
Alexander-Let <settingsdevelopment5@gmail.com>
2026-08-09 21:34:18 +0300

    start page at top on reload

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,10 @@ import './index.css'
 import App from './App.tsx'
 import { LocaleProvider } from './i18n/LocaleContext'
 
+history.scrollRestoration = 'manual'
+if (location.hash) history.replaceState(null, '', location.pathname + location.search)
+window.scrollTo(0, 0)
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <LocaleProvider>
